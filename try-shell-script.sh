307386242e0f3fb859cac3b57e0fd3ea3a638d95
brmaciel <44143089+brmaciel@users.noreply.github.com>
2022-01-08 13:56:28 -0300

    Create try-shell-script.sh

---
+++ try-shell-script.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+TAG_VERSION=${1:-default}
+
+cd ~/
+pwd
+echo "hello world, $TAG_VERSION"
+cd Documents
+pwd
+cd ~/
+
+#curl https://github.com/brmaciel/clean-swift-template/blob/master/install-clean-swift.sh | bash -s argument1
